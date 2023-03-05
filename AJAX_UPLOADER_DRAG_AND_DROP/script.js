610932148afc210089ae79a50af4baf6a3bf4de0
Hemerson Ballen <hjballent@gmail.com>
2023-03-04 19:35:23 -0500

    Completed Exercise

--- AJAX_UPLOADER_DRAG_AND_DROP/script.js
+++ AJAX_UPLOADER_DRAG_AND_DROP/script.js
@@ -1,11 +1,12 @@
+//We declare the main DOM elements that we are gonna to use.
 const d = document,
   $main = d.querySelector("main"),
   $dropzone = document.querySelector(".drop-zone");
 
+//Create the main function that gonna interact with the server through a PHP script and using the XMLhttpRequest object. This function send the files captured in the drag-zone.
 const uploader = (file) => {
-    //console.log(file);
     const xhr = new XMLHttpRequest(),
-      formData = new FormData();
+      formData = new FormData(); //Create a form-model to append de data and send it to the server
     formData.append("file", file);
     xhr.addEventListener("readystatechange", (e) => {
       if (xhr.readyState !== 4) return;
@@ -20,11 +21,12 @@ const uploader = (file) => {
     });
 
     xhr.open("POST", "assets/uploader.php");
-    xhr.setRequestHeader("enc-type", "multipart/form-data");
+    xhr.setRequestHeader("enc-type", "multipart/form-data"); //Creating the header of the request
     xhr.send(formData);
   },
+  //This function interact with the DOM, creating a progress bar for all the files that was drop on the drag-zone and calling the upload() function for send each file.
   progressUpload = (file) => {
-    console.log(file);
+    //We create the progress bar, its values and inner elements
     const $progress = d.createElement("progress"),
       $span = d.createElement("span");
     $progress.value = 0;
@@ -32,14 +34,18 @@ const uploader = (file) => {
     $main.insertAdjacentElement("beforeend", $progress);
     $main.insertAdjacentElement("beforeend", $span);
 
+    //This 'file reader' interprets the uploading information of every file
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
 
+    //Function that calculate the uploading percentage of each file.
     fileReader.addEventListener("progress", (e) => {
       let progress = parseInt((e.loaded * 100) / e.total);
       $progress.value = progress;
       $span.innerHTML = `<b>${file.name} - ${progress}%</b>`;
     });
+
+    //Deleting the progress (es) bar(s)
     fileReader.addEventListener("loadend", (e) => {
       uploader(file);
       setTimeout(() => {
@@ -49,16 +55,21 @@ const uploader = (file) => {
     });
   };
 
+//Event when the file(s) are dragged over the drag-zone, in this case an animation start when are files over the drag-zone
 $dropzone.addEventListener("dragover", (e) => {
   e.preventDefault();
   e.stopPropagation();
   $dropzone.classList.add("is-active");
 });
+
+//Event when the file(s) leave the drag-zone. The animation ends
 $dropzone.addEventListener("dragleave", (e) => {
   e.preventDefault();
   e.stopPropagation();
   $dropzone.classList.remove("is-active");
 });
+
+//Event when the file(s) are dropped in the drag-zone. The animations ends and the function progressUpload() its called.
 $dropzone.addEventListener("drop", (e) => {
   e.preventDefault();
   e.stopPropagation();
